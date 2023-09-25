51705b5898a7e66bd1a83f11dd524c855ed2e770
Sacha  Lhermet <sacha.lhermet@gmail.com>
2023-09-25 21:57:11 +0200

    Création  d'un nouveau projet versionné Calculator

---
+++ Exo3/src/main/java/com/mycompany/exo3/Exo3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.exo3;
+
+/**
+ *
+ * @author sacha
+ */
+public class Exo3 {
+
+    public static void main(String[] args) {
+        int ;
+        int 
+        
+    
+    
+    
+    
+    
+    }
+}

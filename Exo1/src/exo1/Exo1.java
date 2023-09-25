9b6137bb152596dd11d37a7b84028296f5dec9d8
Sacha  Lhermet <sacha.lhermet@gmail.com>
2023-09-25 10:51:37 +0200

    Création  d'un nouveau projet versionné Exo1

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sacha
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonour, quel est votre prenom ");
+        prenom = sc.nextLine();
+                
+                
+                
+    }
+    
+}
+    
+

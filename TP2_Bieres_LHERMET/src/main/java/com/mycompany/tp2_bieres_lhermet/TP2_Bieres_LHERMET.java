4eb92aa8effa7db08e198285c22f89bea1d51f93
Sacha  Lhermet <sacha.lhermet@gmail.com>
2023-10-06 17:42:34 +0200

    Création  d'un nouveau projet versionné TP2

--- TP2_Bieres_LHERMET/src/main/java/com/mycompany/tp2_bieres_lhermet/TP2_Bieres_LHERMET.java
+++ TP2_Bieres_LHERMET/src/main/java/com/mycompany/tp2_bieres_lhermet/TP2_Bieres_LHERMET.java
@@ -11,21 +11,9 @@ package com.mycompany.tp2_bieres_lhermet;
 public class TP2_Bieres_LHERMET {
 
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere("cuvée des trolls",);
-        uneBiere.Nom = "Cuvée des trolls";
-        uneBiere.degreAlcool = 7.0;
-        uneBiere.brasserie = "Dubuisson";
-        uneBiere.ouverte = false;
-        uneBiere.lireEtiquette();
-
-        BouteilleBiere secondebière = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe");
-        secondebière.Nom = "Leffe";
-        secondebière.degreAlcool = 6.6;
-        secondebière.brasserie = "Abbaye de Leffe";
-        secondebière.ouverte = false;
-        secondebière.lireEtiquette();
+        BouteilleBiere uneBiere = new BouteilleBiere("cuvée des trolls",7.0,"Dubuisson");
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe");
         
-        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
 
 
     }

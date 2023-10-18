01328e0fd6aa8e06f2b5932cdfb255c5873a1048
Sacha  Lhermet <sacha.lhermet@gmail.com>
2023-10-18 15:17:23 +0200

    Création  d'un nouveau projet versionné TP3

---
+++ TP3_Heroic_Fantasy_LHERMET/src/main/java/com/mycompany/tp3_heroic_fantasy_lhermet/TP3_Heroic_Fantasy_LHERMET.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp3_heroic_fantasy_lhermet;
+
+/**
+ *
+ * @author sacha
+ */
+public class TP3_Heroic_Fantasy_LHERMET {
+
+    public static void main(String[] args) {
+        Epee Epee1=new Epee("Excalibur",7,5);
+        Epee Epee2=new Epee("Durandal",4,7);
+        Baton Baton1 =new Baton("Chêne", 4, 5 );
+        Baton Baton2 =new Baton("Charme", 5, 6 );   
+    }
+}

ef3e36300a43cc8bdd0a855833a70c5ee567107c
Sacha  Lhermet <sacha.lhermet@gmail.com>
2023-10-24 12:43:54 +0200

    Création  d'un nouveau projet versionné TP3

--- TP3_Heroic_Fantasy_LHERMET/src/main/java/Personnage/Personnage.java
+++ TP3_Heroic_Fantasy_LHERMET/src/main/java/Personnage/Personnage.java
@@ -19,6 +19,7 @@ abstract public class Personnage {
     }
     int pv;
     Armes armekip=null;
+    
        
 public Personnage(String unNom,int unpv, Armes unarmekip){
     nom=unNom;
@@ -33,7 +34,7 @@ public Personnage(String unNom,int unpv, Armes unarmekip){
 @Override
 public String toString(){
     String chaine_a_retourner;
-    chaine_a_retourner=nom+"debate:"+pv;
+    chaine_a_retourner=nom+"debate:"+pv+armekip;
     System.out.println("chaine_a_retourner");
     return chaine_a_retourner;
      
@@ -50,6 +51,20 @@ ArrayList<Armes> unarmekip =new ArrayList<Armes>();
         stuff.add(nouvelle_arme);
     }
    }
+   public void arme_selectionner(ArrayList<Armes> stuff,ArrayList<Armes> nouvelle_arme){
+       int w;
+       for(int i=0;i<stuff.size();i++){
+           System.out.println(i+":"+stuff.get(i));
+       }
+       scanner sc;
+       sc=new scanner(System.in);
+       w=sc.nextInt();
+       armekip=stuff.get(w);
+}
+
+    public Armes getArmekip() {
+        return armekip;
+    }
 }
 
 
